6ec8e9a040ecac207b72f9870d4d89428ecdb7b0
jjgorton <jjgorton@gmail.com>
2019-09-18 21:54:30 -0400

    ran migrate latest

--- BE/data/migrations/20190917183754_users.js
+++ BE/data/migrations/20190917183754_users.js
@@ -1,5 +1,5 @@
 exports.up = function(knex) {
-	return knex.schema.creatTable('users', (tbl) => {
+	return knex.schema.createTable('users', (tbl) => {
 		tbl.increments();
 
 		tbl.string('username', 128).notNullable().unique();
