9a3d1f3811ec3eb8aaf5bcb5f59fc42db1512607
jjgorton <jjgorton@gmail.com>
2019-11-30 13:45:36 -0500

    took off unique constraint on workspace name

--- data/migrations/20190917192046_workspaces.js
+++ data/migrations/20190917192046_workspaces.js
@@ -1,15 +1,17 @@
 exports.up = function(knex) {
-	return knex.schema.createTable('workspaces', (tbl) => {
-		tbl.increments();
+    return knex.schema.createTable('workspaces', tbl => {
+        tbl.increments();
 
-		tbl.string('name', 128).notNullable().unique();
+        tbl.string('name', 128).notNullable();
 
-		tbl.text('description');
+        tbl.text('description');
 
-		tbl.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
-	});
+        tbl.timestamp('created_at')
+            .notNullable()
+            .defaultTo(knex.fn.now());
+    });
 };
 
 exports.down = function(knex) {
-	return knex.schema.dropTableIfExists('workspaces');
+    return knex.schema.dropTableIfExists('workspaces');
 };
